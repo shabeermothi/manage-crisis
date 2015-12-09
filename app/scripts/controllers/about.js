7e3eb3a6b195145c96a0d3738a4d731dbdd1923d
Shabeer Mothi <shabeermothi@theshabeer.com>
2015-12-09 01:09:11 -0500

    Added app images, stylesheets, views and angular modules

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name managecrisisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the managecrisisApp
+ */
+angular.module('managecrisisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name managecrisisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the managecrisisApp
+ */
+angular.module('managecrisisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
